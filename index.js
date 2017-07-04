56452fe84d1a634149678e8ae5023919d26dad93
stetsmando <stetsmando@gmail.com>
2017-07-03 19:33:15 -0600

    Add debug message

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ class MotionDetectionModule extends EventEmitter {
 
     videoCaptureChild.on('message', (message) => {
       if (message.error) {
+        console.log(`Error in video capture: ${ message.error }`);
         self.emit('error', message.error);
       }
       else {
